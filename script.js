497907b8981d51dd1818504e38560fc0f6b29c5a
Yash Khalate <yashkhalate8799845458@gmail.com>
2025-10-04 15:29:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+ScrollReveal().reveal('section h2', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  reset: true
+});
+
+ScrollReveal().reveal('.skills-grid .skill, .projects-grid .project-card',{
+  interval: 200,
+  duration: 800,
+  scale: 0.9,
+  reset: true
+});
